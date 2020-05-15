ba77b43eedcf64a079bea5600ba4315fad883eef
firzok <firzok.nadeem@gmail.com>
2020-05-15 23:06:04 +0500

    added some comments

--- subagentObject.c
+++ subagentObject.c
@@ -1,9 +1,9 @@
+#include <stdlib.h>
+#include "subagentObject.h"
+#include <postgresql/libpq-fe.h>
 #include <net-snmp/net-snmp-config.h>
 #include <net-snmp/net-snmp-includes.h>
 #include <net-snmp/agent/net-snmp-agent-includes.h>
-#include "subagentObject.h"
-#include <postgresql/libpq-fe.h>
-#include <stdlib.h>
 
 void init_version()
 {
@@ -15,6 +15,9 @@ void init_version()
 
     netsnmp_handler_registration *reginfo;
 
+    snmp_log(LOG_INFO, "Initalizing Version instance.\n");
+
+    //Creates a handler registration structure with a new MIB handler.
     reginfo = netsnmp_create_handler_registration("version", NULL,
                                                   nstAgentSubagentObject_oid,
                                                   OID_LENGTH(nstAgentSubagentObject_oid),
@@ -26,14 +29,17 @@ void init_version()
                                ASN_OCTET_STR, watcher_flags,
                                sizeof(version), NULL);
 
+    //netsnmp_register_watched_instance is used to register the static string variable "version"
     netsnmp_register_watched_instance(reginfo, &watcher_info);
+
+    snmp_log(LOG_INFO, "Done initalizing Version instance.\n\n");
 }
 
 static long logSize;
 FILE *ls;
 int handler_logSize(netsnmp_mib_handler *handler, netsnmp_handler_registration *reginfo, netsnmp_agent_request_info *reqinfo, netsnmp_request_info *requests)
 {
-    printf("Log size accessed.\n");
+    snmp_log(LOG_INFO, "Log size accessed.\n");
 
     switch (reqinfo->mode)
     {
@@ -46,6 +52,7 @@ int handler_logSize(netsnmp_mib_handler *handler, netsnmp_handler_registration *
 
         logSize = strtol(sizeofLog, NULL, 10);
 
+        //snmp_set_var_typed_value is used to update the value of "logSize"
         snmp_set_var_typed_value(requests->requestvb, ASN_INTEGER,
                                  &logSize,
                                  sizeof(logSize));
@@ -74,9 +81,17 @@ void init_log_size()
 
     snmp_log(LOG_INFO, "Initalizing Log Size instance.\n");
 
-    netsnmp_register_long_instance("nstAgentSubagentObject", log_size_oid, OID_LENGTH(log_size_oid), &logSize, handler_logSize);
+    /*
+    * the line below registers the "logSize" variable above as
+    * accessible.
+    * 
+    * handler_logSize is a callback function defined above that 
+    * be called everytime "signalValue" is accessed, it also acts 
+    * as an updater of this value.
+    */
+    netsnmp_register_long_instance("logSize", log_size_oid, OID_LENGTH(log_size_oid), &logSize, handler_logSize);
 
-    snmp_log(LOG_INFO, "Done initalizing Log Size instance.\n");
+    snmp_log(LOG_INFO, "Done initalizing Log Size instance.\n\n");
 }
 
 static long signalValue;
@@ -84,7 +99,7 @@ static long signalValue;
 void update_signal_value()
 {
     /*
-     * Getting the latest value of signalValue from DB
+     * Getting the latest value of signalValue from DB and setting it
      * In case of any error default value is set to 0
      */
 
@@ -125,13 +140,14 @@ void update_signal_value()
 
 int handler_signalValue(netsnmp_mib_handler *handler, netsnmp_handler_registration *reginfo, netsnmp_agent_request_info *reqinfo, netsnmp_request_info *requests)
 {
-    printf("Signal Value accessed.\n");
+    snmp_log(LOG_INFO, "Signal Value accessed.\n");
 
     switch (reqinfo->mode)
     {
     case MODE_GET:
         update_signal_value();
 
+        //snmp_set_var_typed_value is used to update the value of "signalValue"
         snmp_set_var_typed_value(requests->requestvb, ASN_INTEGER,
                                  &signalValue,
                                  sizeof(signalValue));
@@ -149,9 +165,17 @@ void init_db_signalValue()
 
     static oid signalValue_oid[] = {1, 3, 6, 1, 4, 1, 53864, 2, 4, 1, 1, 2, 1};
 
-    snmp_log(LOG_INFO, "Initalizing signalValue instance.\n");
+    snmp_log(LOG_INFO, "Initalizing Signal Value instance.\n");
 
+    /*
+    * the line below registers the "signalValue" variable above as
+    * accessible.
+    * 
+    * handler_signalValue is a callback function defined above that 
+    * be called everytime "signalValue" is accessed, it also acts 
+    * as an updater of this value.
+    */
     netsnmp_register_long_instance("signalValue", signalValue_oid, OID_LENGTH(signalValue_oid), &signalValue, handler_signalValue);
 
-    snmp_log(LOG_INFO, "Done initalizing signalValue instance.\n");
+    snmp_log(LOG_INFO, "Done initalizing Signal Value instance.\n\n");
 }
